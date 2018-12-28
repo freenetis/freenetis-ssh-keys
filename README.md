70b03c3573ffcc9f32f08e4cda44ef095c173d58
Ondřej Fibich <fibich@freenetis.org>
2018-12-28 21:54:16 +0100

    Adds CI test that builds DEB package, check it, install it and run it on all supported DEBIANs.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 FreenetIS SSH keys
 ==================
 
+[![Build Status](https://travis-ci.org/freenetis/freenetis-ssh-keys.svg?branch=travis-ci)](https://travis-ci.org/freenetis/freenetis-ssh-keys)
+
 Synchronize public SSH keys of device admins in order to allow access via SSH to the device that is specified by his FreenetIS ID.
 
 For more informations see <http://wiki.freenetis.org/index.php/Instalace#Bal.C3.ADk_freenetis-ssh-keys>.
---
+++ test/install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+if [ $# -ne 1 ]; then
+	echo "usage: $0 DEB"
+	exit 2
+fi
+
+DEB="$1"
+UNIT=/etc/init.d/freenetis-ssh-keys
+CONFIG=/etc/freenetis/freenetis-ssh-keys.conf
+
+export DEBIAN_FRONTEND=noninteractive
+
+echo "Install deb package"
+apt-get update -q
+if apt --version >/dev/null 1>&2; then
+	apt install -q -y "$DEB"
+else
+	dpkg -i "$DEB" || apt-get install -q -y -f
+fi
+
+echo "Util test"
+$UNIT version
+
+$UNIT | grep "Download failed (code:"
+
+rm $CONFIG
+$UNIT | grep "Config file is missing at path $CONFIG. Terminating..."
